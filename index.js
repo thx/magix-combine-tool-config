bd9887ea7520520bdbd7ba695dc3349bb2a1ef4a
yujia <yujia.yjq@alibaba-inc.com>
2018-08-16 11:56:18 +0800

    update gallery mxMap

--- index.js
+++ index.js
@@ -64,11 +64,16 @@ module.exports = (magixCliConfig, customConfig = {}) => {
                     content = content.replace(/<table/g, '<div>$&').replace(/<\/table>/g, '$&</div>');
                     return `<${ctrl.tag} mx-view="${tag.mxView}" ${ctrl.attrs} ${ctrl.viewAttrs}>${content}</${ctrl.tag}>`;
                 },
-                'mx-table.list'(tag) {
-                    let content = tag.content;
-                    let ctrl = tag.seprateAttrs('div');
-                    content = content.replace(/<table/g, '<div>$&').replace(/<\/table>/g, '$&</div>');
-                    return `<${ctrl.tag} mx-view="${tag.mxView}" ${ctrl.attrs} ${ctrl.viewAttrs}>${content}</${ctrl.tag}>`;
+                'mx-loading'() {
+                    return `<div class="loading">
+                            <span class="loading-anim"></span>
+                        </div>`;
+                },
+                'mx-dropdown.item'(i) {
+                    return `<i ${i.attrs} class="none">${i.content}</i>`;
+                },
+                'mx-carousel.panel'(i) {
+                    return `<div ${i.attrs} style="position: absolute;" class="carousel-panel">${i.content}</div>`;
                 }
             }
         },
